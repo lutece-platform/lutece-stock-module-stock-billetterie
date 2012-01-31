32505ecf2b8b1fb83f320a76eca9c6fdf1e5d0e9
NMO-SOPRA <NMO-SOPRA@users.noreply.github.com>
2012-01-31 13:03:29 +0000

    Update sql scripts
    git-svn-id: https://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/module-stock-billetterie@37667 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/stock/modules/billetterie/plugin/init_db_stock_billetterie.sql
+++ src/sql/plugins/stock/modules/billetterie/plugin/init_db_stock_billetterie.sql
@@ -6,13 +6,3 @@
 INSERT INTO `billetterie_portlet` (`id_portlet`, `number_show`, `type_content_portlet`) VALUES
 (98, 8, 'a-laffiche'),
 (99, 5, 'a-venir');
-
-
---
--- Contenu de la table `stock_offer_genre`
---
-
-INSERT INTO `stock_offer_genre` (`id_offer_genre`, `name`) VALUES
-(1, 'Tarif réduit'),
-(2, 'Invitation'),
-(3, 'Invitation spectacle enfants');
