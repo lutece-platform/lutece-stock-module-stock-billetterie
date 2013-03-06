5484e9ff49a0fcf4c534e7bdacfb4ba4e6c4afcd
ABA <ABA@bab10101-e421-0410-a517-8ce0973de3ef>
2013-03-06 14:49:17 +0000

    Bin folder commited by mistake
    git-svn-id: https://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/module-stock-billetterie@47257 bab10101-e421-0410-a517-8ce0973de3ef

--- bin/sql/plugins/stock/modules/billetterie/plugin/init_db_stock_billetterie.sql
+++
@@ -1,8 +0,0 @@
-
---
--- Contenu de la table `billetterie_portlet`
---
-
-INSERT INTO `billetterie_portlet` (`id_portlet`, `number_show`, `type_content_portlet`) VALUES
-(98, 8, 'a-laffiche'),
-(99, 5, 'a-venir');
--- bin/sql/plugins/stock/plugin/init_db_stock_billetterie.sql
+++
@@ -1,8 +0,0 @@
---
--- Contenu de la table `stock_offer_genre`
---
-
-INSERT INTO `stock_offer_genre` (`id_offer_genre`, `name`) VALUES
-(1, 'Tarif réduit'),
-(2, 'Invitation'),
-(3, 'Invitation spectacle enfants');
