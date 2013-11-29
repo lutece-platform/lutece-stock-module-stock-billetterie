64d66843af8b9b17cbb5b7cd70d975747bea4a8b
jchaline <jchaline@users.noreply.github.com>
2013-11-29 16:28:27 +0000

    Remove wrong packages
    git-svn-id: https://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/module-stock-billetterie@54485 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/stock/modules/billetterie/utils/ImageUtils.java
+++ src/java/fr/paris/lutece/plugins/stock/modules/billetterie/utils/ImageUtils.java
@@ -1,14 +1,5 @@
 package fr.paris.lutece.plugins.stock.modules.billetterie.utils;
 
-import java.awt.Container;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.MediaTracker;
-import java.awt.RenderingHints;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public final class ImageUtils
 {
