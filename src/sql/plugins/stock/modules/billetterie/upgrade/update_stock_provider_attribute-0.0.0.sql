e56b4ea00d0bf55ad991287b92a79410d0b25c92
jchaline <jchaline@users.noreply.github.com>
2013-12-02 15:13:50 +0000

    Move SQL script to the upgrade script 1.0.7->1.0.8
    git-svn-id: https://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/module-stock-billetterie@54562 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/stock/modules/billetterie/upgrade/update_stock_provider_attribute-0.0.0.sql
+++
@@ -1,6 +0,0 @@
---
--- update les noms des attributs utilisés par l'application
---
-UPDATE `billetterie`.`stock_provider_attribute` SET `attribute_key` = 'contactName1' WHERE  `stock_provider_attribute`.`attribute_key` = 'contactName';
-UPDATE `billetterie`.`stock_provider_attribute` SET `attribute_key` = 'phoneNumber1' WHERE  `stock_provider_attribute`.`attribute_key` = 'phoneNumber';
-UPDATE `billetterie`.`stock_provider_attribute` SET `attribute_key` = 'mail1' WHERE  `stock_provider_attribute`.`attribute_key` = 'mail';
